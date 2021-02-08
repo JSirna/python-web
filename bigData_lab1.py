2a3dd1715d665f8d006419792989cedfeb882706
JSirna <whitewolfhunter13@yahoo.com>
2021-02-08 17:52:16 -0500

    added test file; editing lab1

--- bigData_lab1.py
+++ bigData_lab1.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import mysql.connector 
 from mysql.connector import (connection)
 
+URL = 'https://www.infoplease.com/primary-sources/government/presidential-speeches/state-union-addresses'
+
 try:
   cnx = mysql.connector.connect(user='testUser',
                                 database='DataExtraction')
@@ -15,3 +17,21 @@ except mysql.connector.Error as err:
     print(err)
 else:
   cnx.close()
+
+#get request
+response = requests.get(URL)
+response.encoding = 'utf-8'
+
+#check to see if response returned successful
+if (response.status_code == 200):
+    print ('get request successful')
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    bodyContent = soup.find_all('span', class_='article') #find_all returns an iterable; find just returns a single element
+
+    for elements in bodyContent:
+      links = elements.find('a')['href']
+      #print (links)
+      # if None in links:
+      #     continue
+      print (links)
