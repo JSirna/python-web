04c63890e01552dadfbef3d13bf66ef0150bffb8
jsirna <whitewolfhunter13@yahoo.com>
2021-02-13 11:41:33 -0500

    edited some things

--- wiki.py
+++ wiki.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 #URL hard-coded
-URL = 'https://en.wikipedia.org/wiki/COVID-19_pandemic'
+URL = 'https://en.wikipedia.org/wiki/Bookkeeping'
 
 #get request
 response = requests.get(URL)
@@ -11,13 +11,14 @@ response.encoding = 'utf-8'
 #check to see if response returned successful
 if (response.status_code == 200):
     print ('get request successful')
-    soup = BeautifulSoup(response.content, 'html.parser')
-    #bodyContent = soup.find(id='mw-content-text')
-    bodyContent = soup.find_all('div', class_='mw-parser-output') #find_all returns an iterable; find just returns a single element
-    #print (bodyContent.prettify())
+    soup = BeautifulSoup(response.content, 'lxml')
+    
+    bodyContent = soup.find_all('div', class_='mw-parser-output') 
+    #find_all returns an iterable; find just returns a single element
+    tags = soup.find_all(True)
     with open('data.txt','w') as f:
         for element in bodyContent:
-            pars = element.find_all('p')
+            pars = element.find_all(['p','h1','h2','h3','ul'])
             if None in pars:
                 continue
             for p in pars:
@@ -30,12 +31,3 @@ elif (response.status_code == 204):
     print('no content found')
 else:
     print ('not found')
-
-#response.content returns the raw bytes
-#print (response.content)
-
-#response.text just returns the text
-
-#print (response.text)
-
-#response.json() returns a dictionary -> can obtain values of objects by key
