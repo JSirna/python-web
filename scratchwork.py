8be355c60c57c842d9c86259bd29825e3205145c
JSirna <jsirna33@gmail.com>
2021-02-24 19:45:08 -0500

    updated scratchwork

--- scratchwork.py
+++ scratchwork.py
@@ -1,6 +1,24 @@
-import csv
-with open('data.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',',
-                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
-    spamwriter.writerow(['Name'] + ['Date'] + ['Name2'] + ['Date2'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+#use for the dirty links behind the scenes
+
+import requests
+from bs4 import BeautifulSoup
+
+#URL hard-coded
+URL = 'http://www.thecramped.com/what-we-use/'
+
+#get request
+response = requests.get(URL)
+response.encoding = 'utf-8'
+
+if (response.status_code == 200):
+    print ('get request successful')
+    soup = BeautifulSoup(response.content, 'lxml')
+    with open('linkScraper.html','w') as f:
+        for stuff in soup:
+            print(stuff)
+            f.write(str(stuff))
+    f.close()
+elif (response.status_code == 204):
+    print('no content found')
+else:
+    print ('not found')
